e86f27b4f87e8b836144148f1f3c3ec44648649f
unknown <you@example.com>
2022-09-28 05:53:27 +0530

    Added : board

--- src/com/bridgelabz/TicTacToe/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToe/TicTacToeGame.java
@@ -1,5 +1,17 @@
 package com.bridgelabz.TicTacToe;
 
-public class TicTacToeGame {
+import java.util.Arrays;
 
+public class TicTacToeGame {
+    private static final char EMPTY = ' ';
+    private char[] board;
+
+    TicTacToeGame() {
+        board = new char[10];
+        Arrays.fill(board,EMPTY);
+    }
+
+    public static void main(String[] args) {
+        TicTacToeGame game = new TicTacToeGame();
+    }
 }
