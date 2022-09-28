81b165c151cc0912425324779f51a4dc660f8ec8
unknown <you@example.com>
2022-09-28 07:34:10 +0530

    Added : Choose a letter 'X' or 'O'

--- src/com/bridgelabz/TicTacToe/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToe/TicTacToeGame.java
@@ -4,14 +4,49 @@ import java.util.Arrays;
 
 public class TicTacToeGame {
     private static final char EMPTY = ' ';
+    private static final char CROSS = 'X';
+    private static final char ROUND = 'O';
     private char[] board;
+    private char playerSymbol;
+    private char computerSymbol;
+
 
     TicTacToeGame() {
         board = new char[10];
         Arrays.fill(board,EMPTY);
     }
 
+    private TicTacToeGame(char[] board, char playerSymbol, char computerSymbol) {
+        this.board = board;
+        this.playerSymbol = playerSymbol;
+        this.computerSymbol = computerSymbol;
+
+    }
+
+    public char getPlayerSymbol() {
+        return playerSymbol;
+    }
+
+    public char getComputerSymbol() {
+        return computerSymbol;
+    }
+
+
+    public void choosePlayerSymbol(char playerSymbol) {
+        if (playerSymbol == CROSS) {
+            this.playerSymbol = CROSS;
+            this.computerSymbol = ROUND;
+        } else if (playerSymbol == ROUND) {
+            this.playerSymbol = ROUND;
+            this.computerSymbol = CROSS;
+        } else
+            System.out.println("Invalid Symbol");
+    }
+
+
     public static void main(String[] args) {
         TicTacToeGame game = new TicTacToeGame();
+        System.out.println("Player symbol is: " + game.getPlayerSymbol());
+        System.out.println("initial:");
     }
 }
